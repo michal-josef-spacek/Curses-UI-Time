4ca1613107ff6c7c86fe0fb6c95c4e6ee27ba597
Michal Špaček <tupinek@gmail.com>
2014-08-07 18:24:04 +0200

    Prepare for 0.05 version.

--- Time.pm
+++ Time.pm
@@ -34,7 +34,7 @@ Readonly::Scalar our $WIDTH_SEC => 52;
 Readonly::Scalar our $WIDTH_SPACE => 1;
 
 # Version.
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 # Constructor.
 sub new {
@@ -491,6 +491,6 @@ tty-clock program.
 
 =head1 VERSION
 
-0.04
+0.05
 
 =cut
